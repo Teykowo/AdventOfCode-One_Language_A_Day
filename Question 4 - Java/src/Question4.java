7f28a8e534082334a6583aba38b2116f2fdcb3f7
Teyko <lepennecguilian@gmail.com>
2023-01-06 16:46:16 +0100

    4th day update

--- Question 4 - Java/src/Question4.java
+++ Question 4 - Java/src/Question4.java
@@ -3,17 +3,24 @@ package src;
 
 // Import java's Input/Output library.
 import java.io.*;
+// Import the logger library.
+import java.util.logging.Logger;
+import java.util.logging.Level;
 
 // Define the main class, with the file's name.
-public class Question_4{
+public class Question4{
     // Define the main function which is called when the file is called.
     public static void main(String[] args) {
-        try{
-            // Get the file name from the first parameter.
-            File file = new File(args[0]);
+        final Logger loggerObject = Logger.getLogger( "logs" );
+        // Initialize a str version of the answer to use in the logger.
+        String answerStr = "";
 
-            // Read the file by instantiating a FileReader object.
-            FileReader fileReader = new FileReader(file);
+        // Get the file name from the first parameter.
+        File file = new File(args[0]);
+        
+        // Read the file by instantiating a FileReader object.
+        try (FileReader fileReader = new FileReader(file)) {
+            
             // Buffer the text by instantiating a BufferedReader object.
             BufferedReader bufferedReader = new BufferedReader(fileReader);
 
@@ -29,16 +36,16 @@ public class Question_4{
                 assignments = line.split(",");
 
                 // Get the lower and upper bounds of each assignment as ints by splitting at the hyphen seperator.
-                int lowerBoundA = (int) Integer.parseInt(assignments[0].split("-")[0]);
-                int UpperBoundA = (int) Integer.parseInt(assignments[0].split("-")[1]);
-                int lowerBoundB = (int) Integer.parseInt(assignments[1].split("-")[0]);
-                int UpperBoundB = (int) Integer.parseInt(assignments[1].split("-")[1]);
+                int lowerBoundA = Integer.parseInt(assignments[0].split("-")[0]);
+                int upperBoundA = Integer.parseInt(assignments[0].split("-")[1]);
+                int lowerBoundB = Integer.parseInt(assignments[1].split("-")[0]);
+                int upperBoundB = Integer.parseInt(assignments[1].split("-")[1]);
 
                 //  if any the first assignments bouds are between the second's or vice versa.
-                if ((lowerBoundA >= lowerBoundB && lowerBoundA <= UpperBoundB) || 
-                    (UpperBoundA >= lowerBoundB && UpperBoundA <= UpperBoundB) ||
-                    (lowerBoundB >= lowerBoundA && lowerBoundB <= UpperBoundA) || 
-                    (UpperBoundB >= lowerBoundA && UpperBoundB <= UpperBoundA))  {
+                if ((lowerBoundA >= lowerBoundB && lowerBoundA <= upperBoundB) || 
+                    (upperBoundA >= lowerBoundB && upperBoundA <= upperBoundB) ||
+                    (lowerBoundB >= lowerBoundA && lowerBoundB <= upperBoundA) || 
+                    (upperBoundB >= lowerBoundA && upperBoundB <= upperBoundA))  {
                     // Increase the answer's value by 1.
                     answer += 1;
                 } 
@@ -46,12 +53,17 @@ public class Question_4{
             // Close the reader.
             bufferedReader.close();
 
+            // Use a conditional statement before converting the answer to string to avoid the "Invoke method(s) only conditionally" warning.
+            if(answer != null){
+                answerStr = answer.toString();
+            }
             // Print the answer.
-            System.out.print(answer);
+            loggerObject.log(Level.INFO, answerStr);
+
         // If the file is not found or not given:
         } catch (Exception fileNotFounException) {
             // Return an error message informing the user that an argument is needed.
-            System.out.print("File not found. Be sure to include the file's path as argument.");
+            loggerObject.log(Level.SEVERE, "File not found. Be sure to include the file’s path as argument.");
         }
     }
 }
